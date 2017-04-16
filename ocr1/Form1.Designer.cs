d1d7e957e2e898cabdf64b1ed76dfcdd74bf7266
Rahul Kumar <Rahul Kumar>
2017-04-16 17:54:38 +0530

    First Commit

---
+++ ocr1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ocr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.segmentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.segmentationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1354, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // segmentationToolStripMenuItem
+            // 
+            this.segmentationToolStripMenuItem.Name = "segmentationToolStripMenuItem";
+            this.segmentationToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.segmentationToolStripMenuItem.Text = "Segmentation";
+            this.segmentationToolStripMenuItem.Click += new System.EventHandler(this.segmentationToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 171);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(26, 284);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 171);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(664, 67);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(300, 171);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(343, 67);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(300, 171);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(343, 284);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(300, 171);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(664, 284);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(300, 171);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 543);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem segmentationToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ ocr1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Imaging;
+using Accord.Imaging.Filters;
+using Accord.Imaging.Converters;
+
+namespace ocr1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Bitmap original;
+        Bitmap thresholdimage;
+        Bitmap grayimage;
+        Bitmap newImage;
+        Bitmap newImage1;
+        Bitmap newImage3;
+        Bitmap newImage4;
+        Bitmap invertimage;
+        Bitmap invertimage1;
+        Bitmap blobimage;
+        int count=0;
+        Bitmap invertimage2;
+        Bitmap invertimage3;
+        Bitmap blobimage1;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd1 = new OpenFileDialog();
+            if (fd1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap img = new Bitmap(fd1.FileName);
+                img.GetPixel(10, 5);
+                original = img;
+                pictureBox1.Image = original;
+            }
+        }
+
+        private void segmentationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Grayscale grayscale = new GrayscaleBT709();
+            grayimage = grayscale.Apply(original);
+            pictureBox2.Image = grayimage;
+            Threshold bw = new Threshold();
+            thresholdimage = bw.Apply(grayimage);
+            pictureBox2.Image = thresholdimage;
+          //  ResizeBicubic resize = new ResizeBicubic(200, thresholdimage.Height);
+          //  thresholdimage = resize.Apply(thresholdimage);
+            List<int> xC = new List<int> ();
+            int xPrev = 0;
+            for (int i = 0; i < thresholdimage.Width; i++)
+            {
+                count = 0;
+                for (int j = 0; j < thresholdimage.Height; j++)
+                {
+                    Color p = thresholdimage.GetPixel(i, j);
+                    if (p.R == 0 && p.G == 0 && p.B == 0)
+                    {
+                        count++;
+                    }
+                  
+                }
+                if(count != 0) 
+                {
+                    if (xC.Count == 0)
+                    {
+                        xC.Add(i);
+                    }
+                    else
+                    {
+                        if (i - xPrev > 1)
+                        {
+                            xC.Add(i);
+                        }
+                    }
+                    xPrev = i;
+                }
+            }
+            List<Bitmap> ni = new List<Bitmap>();
+            for (int i = 0; i <xC.Count; i++)
+            {
+                int endbound;
+                if (i + 1 >= xC.Count)
+                {
+                    endbound = thresholdimage.Width;
+                }
+                else
+                {
+                    endbound = xC[i + 1];
+                }
+               Crop cr = new Crop(new Rectangle(xC[i], 0, endbound - xC[i], thresholdimage.Height));
+                 newImage = cr.Apply(thresholdimage);
+                    ni.Add(newImage);
+                    pictureBox4.Image = newImage;
+                  //  newImage.Save(@"C:\users\rahul\desktop\sampleImage.png");
+                }
+
+            Invert inv = new Invert();
+            invertimage = inv.Apply(newImage);
+            pictureBox5.Image = invertimage;
+
+            ExtractBiggestBlob ebb = new ExtractBiggestBlob();
+            blobimage = ebb.Apply(invertimage);
+            pictureBox5.Image = blobimage;
+
+            Invert inv1 = new Invert();
+            invertimage1 = inv1.Apply(blobimage);
+            pictureBox5.Image = invertimage1;
+
+            List<int> yC = new List<int>();
+            int xPrev1 = 0;
+            for (int j = 0; j < thresholdimage.Height; j++)
+            {
+                count = 0;
+                for (int i = 0; i < thresholdimage.Width; i++)
+                {
+                    Color p = thresholdimage.GetPixel(i, j);
+                    if (p.R == 0 && p.G == 0 && p.B == 0)
+                    {
+                        count++;
+                    }
+
+                    if (count != 0)
+                    {
+                        if (yC.Count == 0)
+                        {
+                            yC.Add(j);
+                        }
+                        else
+                        {
+                            if (j - xPrev1 > 1)
+                            {
+                                yC.Add(j);
+                            }
+                        }
+                        xPrev1 = j;
+                    }
+                }
+            }
+            List<Bitmap> ni1 = new List<Bitmap>();
+            for (int j = 0; j < yC.Count; j++)
+            {
+                int endbound;
+                if (j + 1 >= yC.Count)
+                {
+                    endbound = thresholdimage.Height;
+                }
+                else
+                {
+                    endbound = yC[j + 1];
+                }
+                Crop cr = new Crop(new Rectangle(0, yC[j], thresholdimage.Height, endbound - yC[j]));
+                newImage3 = cr.Apply(thresholdimage);
+                ni1.Add(newImage3);
+                pictureBox3.Image = newImage3;
+                // newImage.Save(@"C:\users\rahul\desktop\sampleImage1.png");
+            }
+            Invert inv2 = new Invert();
+            invertimage3 = inv2.Apply(newImage3);
+            pictureBox6.Image = invertimage3;
+
+            ExtractBiggestBlob ebb1 = new ExtractBiggestBlob();
+            blobimage1 = ebb1.Apply(invertimage3);
+            pictureBox6.Image = blobimage1;
+
+            Invert inv3 = new Invert();
+            invertimage2 = inv3.Apply(blobimage1);
+            pictureBox6.Image = invertimage2; 
+        }                
+    }
+ }
